182893e1e85425e7307c13c55908a68a23e5c81b
JeanFrançois Tremblay <jf_tremblay"@msn.com>
2020-02-24 13:01:11 -0500

    premier commit

---
+++ wpf_intra/intra_models/intra_model.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace intra_models
+{
+    public class Customer : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public string Name { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Addresse { get; set; }
+
+        public string City { get; set; }
+
+        public string Province { get; set; }
+
+        public string UPC { get; set; }
+
+
+
+
+    }
+}
